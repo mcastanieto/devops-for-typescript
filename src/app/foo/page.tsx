d380a195f9f6d0b0488408c8da8bef68fb497401
Michael Castanieto <michael.castanieto@giantmachines.com>
2024-01-09 11:25:30 -0500

    add foo page

---
+++ src/app/foo/page.tsx
@@ -0,0 +1,3 @@
+export default function Foo() {
+	return <h1>hello world from the foo page!</h1>;
+    };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div>
+      <Link href="/foo">go to foo page!</Link>
+      </div>
+
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
